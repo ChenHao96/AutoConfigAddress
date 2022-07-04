a8498ad37ab2c829744e12d009bab1b2a038204c
StevenChen <949727157@qq.com>
2022-07-04 13:07:38 +0800

    Update UpdateDomainAddress.java

--- src/main/java/com/example/java/UpdateDomainAddress.java
+++ src/main/java/com/example/java/UpdateDomainAddress.java
@@ -6,14 +6,15 @@ public class UpdateDomainAddress {
 
     public static void main(String[] args) throws IOException {
         System.out.println("开始自动化配置...");
-        TelecomIPApplication telecomIPApplication = new TelecomIPApplication();
+        TelecomGatewayApplication telecomGatewayApplication = new TelecomGatewayApplication();
+        telecomGatewayApplication.loginGateway();
         System.out.println("\u001B[33m------\t正在获取您的网关IP");
-        TelecomIPApplication.IPTuple ipTuple = telecomIPApplication.loadWLanNATInfo();
-        System.out.printf("\u001B[0m\u001B[33m------\tWLAN:\t%s\n", ipTuple.getWANIP());
-        System.out.printf("------\tWLANv6:\t%s\n", ipTuple.getWANIPv6());
+        TelecomGatewayApplication.GatewayInfo gatewayInfo = telecomGatewayApplication.getGatewayInfo();
+        System.out.printf("\u001B[0m\u001B[33m------\tWLAN:\t%s\n", gatewayInfo.getWANIP());
+        System.out.printf("------\tWLANv6:\t%s\n", gatewayInfo.getWANIPv6());
         System.out.println("\u001B[0m\u001B[30m------\t正在更新域名地址");
-        AliyunSDKApplication aliyunSDKApplication = new AliyunSDKApplication();
-        aliyunSDKApplication.updateDomainAddress(ipTuple.getWANIP(), ipTuple.getWANIPv6());
+        AliyunDNSApplication aliyunDNSApplication = new AliyunDNSApplication();
+        aliyunDNSApplication.updateDomainAddress(gatewayInfo.getWANIP(), gatewayInfo.getWANIPv6());
         System.out.println("\u001B[0m自动化配置完成.");
     }
 }
