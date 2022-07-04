4b917d5d2e6268b45ee68c588631c560fd7baa51
StevenChen <949727157@qq.com>
2022-07-04 13:07:22 +0800

    Update UpdateSecurityGroup.java

--- src/main/java/com/example/java/UpdateSecurityGroup.java
+++ src/main/java/com/example/java/UpdateSecurityGroup.java
@@ -6,14 +6,14 @@ public class UpdateSecurityGroup {
 
     public static void main(String[] args) throws IOException {
         System.out.println("开始自动化配置...");
-        TelecomIPApplication telecomIPApplication = new TelecomIPApplication();
+        TelecomGatewayApplication telecomGatewayApplication = new TelecomGatewayApplication();
+        telecomGatewayApplication.loginGateway();
         System.out.println("\u001B[33m------\t正在获取您的网关IP");
-        TelecomIPApplication.IPTuple ipTuple = telecomIPApplication.loadWLanNATInfo();
-        System.out.printf("\u001B[0m\u001B[33m------\tWLAN:\t%s\n", ipTuple.getWANIP());
-        System.out.printf("------\tWLANv6:\t%s\n", ipTuple.getWANIPv6());
+        TelecomGatewayApplication.GatewayInfo gatewayInfo = telecomGatewayApplication.getGatewayInfo();
+        System.out.printf("\u001B[0m\u001B[33m------\tWLAN:\t%s\n", gatewayInfo.getWANIP());
         System.out.println("\u001B[0m\u001B[32m------\t正在更新服务器安全组规则");
-        AliyunSDKApplication aliyunSDKApplication = new AliyunSDKApplication();
-        aliyunSDKApplication.updateSecurityGroup(ipTuple.getWANIP());
+        AliyunSecurityGroupApplication aliyunSecurityGroupApplication = new AliyunSecurityGroupApplication();
+        aliyunSecurityGroupApplication.updateSecurityGroup(gatewayInfo.getWANIP());
         System.out.println("\u001B[0m自动化配置完成.");
     }
 }
