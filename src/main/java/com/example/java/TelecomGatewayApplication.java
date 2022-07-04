3cc756dd926d3ae74d383278d3dde38dee3aeeb4
StevenChen <949727157@qq.com>
2022-07-04 17:28:27 +0800

    Update TelecomGatewayApplication.java

--- src/main/java/com/example/java/TelecomGatewayApplication.java
+++ src/main/java/com/example/java/TelecomGatewayApplication.java
@@ -24,53 +24,6 @@ import java.util.regex.Pattern;
 
 public class TelecomGatewayApplication {
 
-    public static void main(String[] args) throws IOException {
-
-        List<String> localLanIPList = new ArrayList<>();
-        Enumeration<NetworkInterface> networkInterfaceEnumeration = NetworkInterface.getNetworkInterfaces();
-        while (networkInterfaceEnumeration.hasMoreElements()) {
-            NetworkInterface networkInterface = networkInterfaceEnumeration.nextElement();
-            if (networkInterface.isUp() && !networkInterface.isLoopback()) {
-                Enumeration<InetAddress> inetAddressEnumeration = networkInterface.getInetAddresses();
-                if (inetAddressEnumeration.hasMoreElements()) {
-                    InetAddress address = inetAddressEnumeration.nextElement();
-                    localLanIPList.add(address.getHostAddress());
-                }
-            }
-        }
-
-        TelecomGatewayApplication telecomGatewayApplication = new TelecomGatewayApplication();
-        System.out.println("------\t正在登录网关");
-        telecomGatewayApplication.loginGateway();
-        System.out.print("\u001B[33m------\t正在获取您的网关IP");
-        System.out.println("\u001B[0m\u001B[31m(暂时仅支持天翼网关 >=3.0)\u001B[0m");
-
-        System.out.println(new Gson().toJson(telecomGatewayApplication.getGatewayInfo()));
-        PortMappingDisplay display = telecomGatewayApplication.getPortMappingDisplay();
-        System.out.println(new Gson().toJson(display));
-
-        localLanIPList.removeIf(s -> !checkIpInRange(s, String.format("%s/%s", display.getLanIp(), display.getMask())));
-        if (localLanIPList.size() > 0) {
-            PortMappingSet portMappingSet = new PortMappingSet();
-            portMappingSet.setSrvname("test");
-            portMappingSet.setOp(PortMappingSetOption.add);
-            if (display.getPmRule() != null && display.getPmRule().size() > 0) {
-                for (PortMappingRule rule : display.getPmRule()) {
-                    if (rule.getDesp().equals(portMappingSet.getSrvname())) {
-                        portMappingSet.setOp(PortMappingSetOption.del);
-                        break;
-                    }
-                }
-            }
-            if (PortMappingSetOption.add.equals(portMappingSet.getOp())) {
-                portMappingSet.setClient(localLanIPList.get(0));
-                portMappingSet.setExPort(8080);
-                portMappingSet.setInPort(8080);
-            }
-            System.out.println(new Gson().toJson(telecomGatewayApplication.portMappingSetSingle(portMappingSet)));
-        }
-    }
-
     private final Properties properties;
 
     private final Set<String> serviceNames = new LinkedHashSet<>();
